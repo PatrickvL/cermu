690749fe017426e3e28bf4bb7e5a04d27cdd8860
PatrickvL <PatrickvL@users.noreply.github.com>
2025-05-25 00:42:06 +0200

    Started removing delegate constructors at runtime

--- aiemu/MOS6510.AddressingModes.cs
+++ aiemu/MOS6510.AddressingModes.cs
@@ -21,7 +21,572 @@ namespace aiemu;
 
 public partial class MOS6510
 {
-    // SHARED ADDRESSING MODE HELPERS
+    // SHARED ADDRESSING MODE HELPERS - REFACTORED TO AVOID RUNTIME DELEGATE ALLOCATION
+    
+    // Immediate addressing modes - specific for each operation
+    private CycleAction ReadImmediateLoadA()
+    {
+        BusRead(PC++);
+        return readImmediateLoadAComplete;
+    }
+
+    private CycleAction ReadImmediateLoadX()
+    {
+        BusRead(PC++);
+        return readImmediateLoadXComplete;
+    }
+
+    private CycleAction ReadImmediateLoadY()
+    {
+        BusRead(PC++);
+        return readImmediateLoadYComplete;
+    }
+
+    private CycleAction ReadImmediateOrAccumulator()
+    {
+        BusRead(PC++);
+        return readImmediateOrAccumulatorComplete;
+    }
+
+    private CycleAction ReadImmediateAndAccumulator()
+    {
+        BusRead(PC++);
+        return readImmediateAndAccumulatorComplete;
+    }
+
+    private CycleAction ReadImmediateXorAccumulator()
+    {
+        BusRead(PC++);
+        return readImmediateXorAccumulatorComplete;
+    }
+
+    private CycleAction ReadImmediateAddWithCarry()
+    {
+        BusRead(PC++);
+        return readImmediateAddWithCarryComplete;
+    }
+
+    private CycleAction ReadImmediateSubtractWithCarry()
+    {
+        BusRead(PC++);
+        return readImmediateSubtractWithCarryComplete;
+    }
+
+    private CycleAction ReadImmediateCompareA()
+    {
+        BusRead(PC++);
+        return readImmediateCompareAComplete;
+    }
+
+    private CycleAction ReadImmediateCompareX()
+    {
+        BusRead(PC++);
+        return readImmediateCompareXComplete;
+    }
+
+    private CycleAction ReadImmediateCompareY()
+    {
+        BusRead(PC++);
+        return readImmediateCompareYComplete;
+    }
+
+    private CycleAction ReadImmediateNop()
+    {
+        BusRead(PC++);
+        return readImmediateNopComplete;
+    }
+
+    private CycleAction ReadImmediateLAX()
+    {
+        BusRead(PC++);
+        return readImmediateLAXComplete;
+    }
+
+    private CycleAction ReadImmediateANC()
+    {
+        BusRead(PC++);
+        return readImmediateANCComplete;
+    }
+
+    private CycleAction ReadImmediateALR()
+    {
+        BusRead(PC++);
+        return readImmediateALRComplete;
+    }
+
+    private CycleAction ReadImmediateARR()
+    {
+        BusRead(PC++);
+        return readImmediateARRComplete;
+    }
+
+    private CycleAction ReadImmediateXAA()
+    {
+        BusRead(PC++);
+        return readImmediateXAAComplete;
+    }
+
+    private CycleAction ReadImmediateAXS()
+    {
+        BusRead(PC++);
+        return readImmediateAXSComplete;
+    }
+
+    // Zero page addressing modes - specific for each operation
+    private CycleAction ReadZeroPageLoadA()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return LoadA();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageLoadX()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return LoadX();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageLoadY()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return LoadY();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageOrAccumulator()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return OrAccumulator();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageAndAccumulator()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return AndAccumulator();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageXorAccumulator()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return XorAccumulator();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageAddWithCarry()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return AddWithCarry();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageSubtractWithCarry()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return SubtractWithCarry();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageCompareA()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return CompareA();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageCompareX()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return CompareX();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageCompareY()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return CompareY();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageBitTest()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return BitTest();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageNop()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return fetchOpcodeAction();
+            };
+        };
+    }
+
+    private CycleAction ReadZeroPageLAX()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(lo);
+            return () => {
+                val = BusComplete();
+                return LAX_Operation();
+            };
+        };
+    }
+
+    // Absolute addressing modes - specific for each operation
+    private CycleAction ReadAbsoluteLoadA()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return LoadA();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteLoadX()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return LoadX();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteLoadY()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return LoadY();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteOrAccumulator()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return OrAccumulator();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteAndAccumulator()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return AndAccumulator();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteXorAccumulator()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return XorAccumulator();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteAddWithCarry()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return AddWithCarry();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteSubtractWithCarry()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return SubtractWithCarry();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteCompareA()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return CompareA();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteCompareX()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return CompareX();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteCompareY()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return CompareY();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteBitTest()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return BitTest();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteNop()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return fetchOpcodeAction();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteLAX()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return LAX_Operation();
+                };
+            };
+        };
+    }
+
+    private CycleAction ReadAbsoluteLAS()
+    {
+        BusRead(PC++);
+        return () => {
+            lo = BusComplete();
+            BusRead(PC++);
+            return () => {
+                hi = BusComplete();
+                addr = (ushort)(lo | (hi << 8));
+                BusRead(addr);
+                return () => {
+                    val = BusComplete();
+                    return LAS_Operation();
+                };
+            };
+        };
+    }
+
+    // Generic addressing mode methods for operations that still need them
     private CycleAction ReadImmediate(Func<CycleAction> operation)
     {
         BusRead(PC++);
