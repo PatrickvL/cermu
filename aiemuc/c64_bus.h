159a0054f224582834db6c8818f908f2cdd3d068
Patrick van Logchen <pvanlogchem@gmail.com>
2025-06-07 11:50:04 +0200

    Rename device_id_per_page into device_id_per_index and change it into a copy instead of a pointer (updated when io port updates pla output)

--- aiemuc/c64_bus.h
+++ aiemuc/c64_bus.h
@@ -38,13 +38,12 @@ static inline int c64_bus_get_device_index(uint16_t address) {
 typedef struct c64_bus_s {
     device_descriptor_t* desc;
     c64_t* c64;
-    uint8_t* device_id_per_page; // Maps each condensed index (32 entries) to a device ID
-    alignas(64) device_access_callback_t device_access_callbacks[16]; // indexed by device ID - unified read/write/context
-    alignas(64) uint8_t device_id_per_index_per_mode[32][32]; // Condensed from 256 to 32 entries per mode
-
-    uint16_t address;       // A0-A15
-    uint8_t  data;          // D0-D7
     uint8_t  control_lines; // R/W, IRQ, NMI, BA, AEC, RDY
+    uint8_t  data;          // D0-D7
+    uint16_t address;       // A0-A15
+    alignas(64) device_access_callback_t device_access_callbacks[16]; // indexed by device ID - unified read/write/context
+    alignas(64) uint8_t device_id_per_index[32]; // Maps each condensed index (32 entries) to a device ID
+    alignas(64) uint8_t device_id_per_index_per_mode[32][32]; // Condensed from 256 to 32 entries per mode
 } c64_bus_t;
 
 void c64_bus_mode_switch(c64_bus_t* c64_bus, uint8_t mode);
