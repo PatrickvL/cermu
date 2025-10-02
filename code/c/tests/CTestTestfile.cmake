762d3722b424a7fc75e7c54281fd795220666ba5
PatrickvL <PatrickvL@users.noreply.github.com>
2025-10-02 08:59:51 +0200

    TMP

--- code/c/tests/CTestTestfile.cmake
+++ code/c/tests/CTestTestfile.cmake
@@ -1,11 +1,37 @@
 # CMake generated Testfile for 
-# Source directory: /home/patrick/Git/aiemu/code/c/tests
-# Build directory: /home/patrick/Git/aiemu/code/c/tests
+# Source directory: D:/Workspaces/Git/aiemu/code/c/tests
+# Build directory: D:/Workspaces/Git/aiemu/code/c/tests
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(processor_tests "/home/patrick/Git/aiemu/code/c/tests/processor_tests_runner" "processor_tests/6502/v1/")
-set_tests_properties(processor_tests PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "/home/patrick/Git/aiemu/code/c/tests" _BACKTRACE_TRIPLES "/home/patrick/Git/aiemu/code/c/tests/CMakeLists.txt;190;add_test;/home/patrick/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
-add_test(fam65xx_cpp_klaus_test "/home/patrick/Git/aiemu/code/c/tests/fam65xx_cpp_klaus_test_runner" "--functional")
-set_tests_properties(fam65xx_cpp_klaus_test PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "/home/patrick/Git/aiemu/code/c/tests/.." _BACKTRACE_TRIPLES "/home/patrick/Git/aiemu/code/c/tests/CMakeLists.txt;204;add_test;/home/patrick/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(processor_tests "D:/Workspaces/Git/aiemu/code/c/tests/Debug/processor_tests_runner.exe" "processor_tests/6502/v1/")
+  set_tests_properties(processor_tests PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests" _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;212;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(processor_tests "D:/Workspaces/Git/aiemu/code/c/tests/Release/processor_tests_runner.exe" "processor_tests/6502/v1/")
+  set_tests_properties(processor_tests PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests" _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;212;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(processor_tests "D:/Workspaces/Git/aiemu/code/c/tests/RelWithDebInfo/processor_tests_runner.exe" "processor_tests/6502/v1/")
+  set_tests_properties(processor_tests PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests" _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;212;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(processor_tests "D:/Workspaces/Git/aiemu/code/c/tests/MinSizeRel/processor_tests_runner.exe" "processor_tests/6502/v1/")
+  set_tests_properties(processor_tests PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests" _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;212;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+else()
+  add_test(processor_tests NOT_AVAILABLE)
+endif()
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test(fam65xx_cpp_klaus_test "D:/Workspaces/Git/aiemu/code/c/tests/Debug/fam65xx_cpp_klaus_test_runner.exe" "--functional")
+  set_tests_properties(fam65xx_cpp_klaus_test PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests/.." _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;226;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test(fam65xx_cpp_klaus_test "D:/Workspaces/Git/aiemu/code/c/tests/Release/fam65xx_cpp_klaus_test_runner.exe" "--functional")
+  set_tests_properties(fam65xx_cpp_klaus_test PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests/.." _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;226;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test(fam65xx_cpp_klaus_test "D:/Workspaces/Git/aiemu/code/c/tests/RelWithDebInfo/fam65xx_cpp_klaus_test_runner.exe" "--functional")
+  set_tests_properties(fam65xx_cpp_klaus_test PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests/.." _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;226;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test(fam65xx_cpp_klaus_test "D:/Workspaces/Git/aiemu/code/c/tests/MinSizeRel/fam65xx_cpp_klaus_test_runner.exe" "--functional")
+  set_tests_properties(fam65xx_cpp_klaus_test PROPERTIES  PASS_REGULAR_EXPRESSION "ALL TESTS PASSED" TIMEOUT "300" WORKING_DIRECTORY "D:/Workspaces/Git/aiemu/code/c/tests/.." _BACKTRACE_TRIPLES "D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;226;add_test;D:/Workspaces/Git/aiemu/code/c/tests/CMakeLists.txt;0;")
+else()
+  add_test(fam65xx_cpp_klaus_test NOT_AVAILABLE)
+endif()
 subdirs("cpu_build")
