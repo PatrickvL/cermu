ddc3f7e1ab9a25fa3523433098be9ee92c8b63b8
PatrickvL <PatrickvL@users.noreply.github.com>
2026-03-13 19:04:41 +0100

    mc6845: implement MMIO bus interface and register chip type

--- src/chip/video/video_registry.cpp
+++ src/chip/video/video_registry.cpp
@@ -10,5 +10,7 @@
 #include "core/chip_registry.hpp"
 
 #include "chip/video/mc6847/mc6847.hpp"
+#include "chip/video/mc6845/mc6845.hpp"
 
 REGISTER_CHIP_TYPE("MC6847", mc6847_t)
+REGISTER_CHIP_TYPE("MC6845", mc6845_t)
